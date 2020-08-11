88bd34fc458c28da7a5547cc8f830c53e656e392
mqp99 <m99excellent@gmail.com>
2020-08-11 13:17:54 +0700

    getmlib

--- document.php
+++ document.php
@@ -1,7 +1,28 @@
 <?php require 'include/header.php' ?>
 	<div class="container">
 		<div class="main-left">
-			ssssssssssss
+			<div class="form-group">
+				<input type="text" class="form-input focus-blue-bold bd-radius" placeholder="Bạn muốn tìm gì ?">
+				<button class="btn"><i class="fal fa-bars"></i></button>
+			</div>
+			<div class="side-menu">
+				<section>
+					<span><b>Getting Started</b></span>
+					<div class="menu">
+						<a href="#" class="active">Giới thiệu</a>
+						<a href="#">Tải xuống</a>
+						<a href="#">Nội dung</a>
+						<a href="#">Trợ năng</a>
+					</div>
+				</section>
+				<section>
+					<span><b>Compunets</b></span>
+					<div class="menu">
+						<a href="#">Button</a>
+						<a href="#">Input group</a>
+					</div>
+				</section>
+			</div>
 		</div>
 		<div class="main-right">
 			sssssssss
--- include/header.php
+++ include/header.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<link rel="stylesheet" href="https://mqp99.github.io/mlib/sass/mlib.min.css">
 	<link rel="stylesheet" href="assets/sass/app.min.css">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.2.0/css/all.css">
 </head>
 <body>
 	<header>
