f5ccf27d46d8b04e69b55542f4c4fa0c35cabfae
mqp99 <m99excellent@gmail.com>
2020-08-13 19:25:22 +0700

    getmlib

--- livecode/js/livecode.js
+++ livecode/js/livecode.js
@@ -1,32 +1,41 @@
 $(function(){
+	var btnCode = $('.btn-code');
 	$('.code-area-html').addClass('active');
-	$('.btn-code').on('click',function(){
+	btnCode.on('click',function(){
 		var _this = $(this).attr('data-type');
+		$(this).siblings().removeClass('selected');
+		$(this).toggleClass('selected');
+		for(i=0;i<=btnCode.length;i++){
+			if(!btnCode.not('.selected')[i]) {
+				if(btnCode.not('.selected').length == 3) {
+					$('.btn-result').attr('disabled',true);
+					$('main').addClass('full-height-view');
+				}else{
+					$('.btn-result').attr('disabled',false);
+					$('main').removeClass('full-height-view');
+				}
+			}
+		}
 		switch (_this) {
 			case 'html':
-				$('.btn-code').siblings().removeClass('active');
-				$(this).addClass('active');
 				$('.code-area-html').addClass('active');
 				$('.code-area-css, .code-area-js').removeClass('active');
 				break;
 			case 'css':
-				$('.btn-code').siblings().removeClass('active');
-				$(this).addClass('active');
 				$('.code-area-css').addClass('active');
 				$('.code-area-html, .code-area-js').removeClass('active');
 				break;
 			case 'js':
-				$('.btn-code').siblings().removeClass('active');
-				$(this).addClass('active');
 				$('.code-area-js').addClass('active');
 				$('.code-area-css, .code-area-html').removeClass('active');
 				break;
 			default:
+				alert('s')
 				break;
 		}
 	})
 	$('.btn-result').on('click',function(){
-		$(this).toggleClass('active');
-		$('.code-area, .preview-area').toggleClass('active');
+		$(this).toggleClass('selected');
+		$('main').toggleClass('full-height-code');
 	})
 })
