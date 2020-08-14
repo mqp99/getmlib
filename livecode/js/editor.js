cd145be7ac7bbbaa4a777e4dad1e4488bc1f51b3
mqp99 <m99excellent@gmail.com>
2020-08-14 14:32:31 +0700

    getmlib

--- livecode/js/editor.js
+++ livecode/js/editor.js
@@ -1,13 +1,11 @@
 $(function() {
-	var valueDOMHTML = "<!-- HTML here, có thể sử dụng key của SublimeText --> \n";
-	var valueDOMCSS = "/* CSS here */ \n";
-	var valueDOMJS = "/* Chưa thể sử dụng được ! */";
+	var times;
 	var editorHTML = CodeMirror.fromTextArea($('#htmlCode')[0], {
 		lineNumbers: true,
 	    tabSize:5,
-	    theme: 'ayu-mirage',
 		mode: "text/html",
    		keyMap: "sublime",
+	    theme: 'ayu-mirage',
    		lint: true,
 		lineWrapping: true,
         autoCloseTags: true,
@@ -21,9 +19,9 @@ $(function() {
 	var editorCSS = CodeMirror.fromTextArea($('#cssCode')[0], {
 		lineNumbers: true,
 	    tabSize:5,
-	    theme: 'ayu-mirage css',
 		mode: "css",
    		keyMap: "sublime",
+	    theme: 'ayu-mirage css',
    		lint: true,
 		lineWrapping: true,
     	autoCloseBrackets: true,
@@ -34,34 +32,52 @@ $(function() {
 	var editorJS = CodeMirror.fromTextArea($('#jsCode')[0], {
 		lineNumbers: true,
 	    tabSize:5,
+   		keyMap: "sublime",
 	    theme: 'ayu-mirage js',
 		mode: "text/javascript",
-   		keyMap: "sublime",
-   		readOnly: 'nocursor',
+   		lint: true,
+   		//readOnly: 'nocursor',
     	autoCloseBrackets: true,
 		lineWrapping: true
 	});
+	/* ============================ 
+		+ SET VALUE CODEMIRROR BEFORE LOAD
+	============================  */
+	var valueDOMHTML = "<!-- HTML here --> \n";
+	var valueDOMCSS = "/* CSS here */ \n";
+	var valueDOMJS = "/* JS here */ \n";
 	editorHTML.setValue(valueDOMHTML);
 	editorCSS.setValue(valueDOMCSS);
 	editorJS.setValue(valueDOMJS);
-	editorHTML.on('keypress',function(){
-		CodeMirror.commands.autocomplete(editorHTML);
-	})
-	editorCSS.on('keypress',function(){
-		CodeMirror.commands.autocomplete(editorCSS);
-	})
-	editorHTML.on('change',function(){ showPreview();})
-	editorCSS.on('change',function(){ showPreview(); })
-	//editorJS.on('change',function(){ showPreview(); })
+	/* ============================ 
+		+ Auto complete HTML, CSS, JS
+	============================  */
+	editorHTML.on('keypress',function(){ /*	CodeMirror.commands.autocomplete(editorHTML); */ })
+	editorCSS.on('keypress',function(){ /* CodeMirror.commands.autocomplete(editorCSS); */ })
+	editorJS.on('keypress',function(){  CodeMirror.commands.autocomplete(editorJS); })
+	/* ============================  
+		+ EVENT CHANGE CODEMIRROR
+	============================  */
+	editorHTML.on('change',function(){ timeoutShowPreview(2000) })
+	editorCSS.on('change',function(){ timeoutShowPreview(2000) })
+	editorJS.on('change',function(){ timeoutShowPreview(2000) })
+	/* ============================  
+		+ FUNCTION CHANGE CODEMIRROR
+	============================  */
+	function timeoutShowPreview(time) {
+		// Clear time out inner Preview if change
+		clearTimeout(times);
+		// Set time out inner Preview
+		times = setTimeout(()=>{ showPreview(); },time) 
+	}
 	function showPreview(){
 		var htmlValue = editorHTML.getValue();
 		var cssValue = `<style>${editorCSS.getValue()}</style>`;
-		//var jsValue = "<scri"+"pt>"+editorJS.getValue('s')+"</scri"+"pt>";
+		var jsValue = "<scri"+"pt type='text/javascript'>"+editorJS.getValue()+"</scri"+"pt>";
 		var frame = $('#preview-window')[0].contentWindow.document;
 		frame.open();
-		frame.close();
+		frame.write(htmlValue,jsValue);
 		$('head',frame).append(cssValue);
-		$('body',frame).append(htmlValue);
-		//$('body',frame).append(jsValue);
+		frame.close();
 	}
 });
